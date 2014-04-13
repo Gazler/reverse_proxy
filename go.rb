e7d1d4a82deb990e99bbfb988feeab857153c5e0
Dan Finnie <dan@danfinnie.com>
2014-04-13 00:56:25 -0400

    Move list of applications from nginx config to environment variable.

--- go.rb
+++ go.rb
@@ -1,10 +1,16 @@
 #! /usr/bin/env ruby
 
 require 'erb'
+require 'yaml'
+require 'ostruct'
 
 $stderr.puts "Starting"
 
-interpolated_conf = ERB.new(File.read('/etc/nginx/nginx.conf.erb')).result
+namespace = OpenStruct.new
+namespace.app_proxies = YAML.load(ENV['APP_PROXIES'])
+namespace_binding = namespace.instance_eval { binding }
+
+interpolated_conf = ERB.new(File.read('/etc/nginx/nginx.conf.erb')).result(namespace_binding)
 File.open('/etc/nginx/nginx.conf', "w") do |f|
   f.print interpolated_conf
 end
