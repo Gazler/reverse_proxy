7dd18a1be35feae53ff7a83fda05486ab555ee8e
Dan Finnie <dan@danfinnie.com>
2014-04-13 01:05:34 -0400

    Move environment variable interpolation into a class.

--- go.rb
+++ go.rb
@@ -6,8 +6,32 @@ require 'ostruct'
 
 $stderr.puts "Starting"
 
+class AppProxy
+  def initialize(ip, port)
+    @ip, @port = ip, port
+  end
+
+  def server_name
+    "#@ip.localhost"
+  end
+
+  def proxy_pass
+    "http://#{host}"
+  end
+
+  private
+
+  def host
+    ENV["#{prefix}ADDR"] + ":" + ENV["#{prefix}PORT"]
+  end
+
+  def prefix
+    "#{@ip.upcase}_1_PORT_#{@port}_TCP_"
+  end
+end
+
 namespace = OpenStruct.new
-namespace.app_proxies = YAML.load(ENV['APP_PROXIES'])
+namespace.app_proxies = YAML.load(ENV['APP_PROXIES']).map { |args| AppProxy.new(*args) }
 namespace_binding = namespace.instance_eval { binding }
 
 interpolated_conf = ERB.new(File.read('/etc/nginx/nginx.conf.erb')).result(namespace_binding)
